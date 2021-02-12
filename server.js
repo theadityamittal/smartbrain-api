069fe74c642e089d3e1c0e398882dfc02940177f
Aditya Mittal <theadityamittal@gmail.com>
2021-02-12 22:41:12 +0530

    trial

--- server.js
+++ server.js
@@ -10,10 +10,12 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      connectionString : process.env.DATABASE_URL,
-      ssl: true
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false
+        }
     }
-});
+})
 
 const app = express();
 
